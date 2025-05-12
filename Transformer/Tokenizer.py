5b0c540d7ba2e894cb2829aadf5c213c3a716495
BrdH7940 <minhhuy31012004@gmail.com>
2025-05-12 15:58:53 +0700

    [Feat] Data Preprocessing

---
+++ Transformer/Tokenizer.py
@@ -0,0 +1,77 @@
+from Config import *
+from Dataset import *
+
+# Training
+import torchtext.datasets as datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+
+# Huggingface datasets and tokenizers
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+def get_all_sentences(dataset, lang):
+    for item in dataset:
+        yield item['translation'][lang]
+
+def get_or_build_tokenizer(config, dataset, lang):
+    # Path where tokenizer for given language is saved
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+
+    if not Path.exists(tokenizer_path):
+        # Build 
+        tokenizer = Tokenizer(WordLevel(unk_token = "[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+
+        # Train
+        trainer = WordLevelTrainer(special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency = 2)
+        tokenizer.train_from_iterator(get_all_sentences(dataset, lang), trainer = trainer)
+
+        # Save
+        tokenizer.save(str(tokenizer_path))
+    else:
+        # Load
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    
+    return tokenizer
+
+def get_dataset(config, split = "train"):
+    dataset_raw = load_dataset(
+        'mt_eng_vietnamese',  
+        "iwslt2015-en-vi", 
+        split = split
+    ).shuffle(seed = 42)
+
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, dataset_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, dataset_raw, config['lang_tgt'])
+
+    # Size ratio (Training, Validation) = (0.9, 0.1)
+    train_ds_size = int(0.9 * len(dataset_raw))
+    val_ds_size = len(dataset_raw) - train_ds_size
+    train_dataset_raw, val_dataset_raw = random_split(dataset_raw, [train_ds_size, val_ds_size])
+
+    train_dataset = BilingualDataset(train_dataset_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_dataset = BilingualDataset(val_dataset_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    # Find the maximum length of each sentence in the source and target sentences
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in dataset_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_dataset, batch_size = config['batch_size'], shuffle = True)
+    val_dataloader = DataLoader(val_dataset, batch_size = 1, shuffle = True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
