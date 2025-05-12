8041505d6477df6e3189f352a8f1efe68394460c
BrdH7940 <minhhuy31012004@gmail.com>
2025-05-12 22:00:13 +0700

    [Fix] Small Fix Dataset and Config

--- Transformer/Dataset.py
+++ Transformer/Dataset.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset
 class BilingualDataset(Dataset):
     def __init__(self, dataset, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
         super().__init__()
+        self.seq_len = seq_len
 
         self.dataset = dataset
         self.tokenizer_src = tokenizer_src
@@ -12,9 +13,9 @@ class BilingualDataset(Dataset):
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['[SOS]'])], dtype = torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['[EOS]'])], dtype = torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['[PAD]'])], dtype = torch.int64)
+        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id(['[SOS]'])], dtype = torch.int64)
+        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id(['[EOS]'])], dtype = torch.int64)
+        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id(['[PAD]'])], dtype = torch.int64)
 
     def __len__(self):
         return len(self.dataset)
